e136052062d2e7e53a8a92d4a356d95b7bd8c2d4
matipascue <matias.pascuetin@hotmail.com>
2022-06-02 02:06:16 -0300

    tercer commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -27,7 +27,8 @@ const nuevoModulo = 21 % 5 === 1;
 
 function devolverString(str) {
   // "Return" la string provista: str
-  // Tu código: Return "str"
+  // Tu código:
+  return str
 }
 
 function suma(x, y) {
